24395c4b3caf8d6b4c1151217d4ad818a64e9448
rdlorenzo1972 <randy.lorenzo@gmail.com>
2022-09-23 00:28:43 -0400

    Completed except bonus

--- module04/script.js
+++ module04/script.js
@@ -1,5 +1,5 @@
-// ******************************* 
-// START HERE IF YOU WANT AN EASIER STARTING POINT FOR THIS ASSIGNMENT
+// *******************************
+// Randy Lorenzo Module 04 Assignment
 // *******************************
 //
 // Module 4 Assignment Instructions.
@@ -20,48 +20,73 @@ Hello Larry
 Hello Paula
 Hello Laura
 Good Bye Jim
-
-WARNING!!! WARNING!!!
-The code does NOT currently work! It is YOUR job to make it work
-as described in the requirements and the steps in order to complete this
-assignment.
-WARNING!!! WARNING!!!
-
 */
 
-// STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU)
-// Wrap the entire contents of script.js inside of an IIFE
-// See Lecture 52, part 2
-// (Note, Step 2 will be done in the SpeakHello.js file.)
 (function () {
+  var names = [
+    "Yaakov",
+    "John",
+    "Jen",
+    "Jason",
+    "Paul",
+    "Frank",
+    "Larry",
+    "Paula",
+    "Laura",
+    "Jim",
+  ];
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
 
-// STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
-// Loop over the names array and say either 'Hello' or "Good Bye"
-// using either the helloSpeaker's or byeSpeaker's 'speak' method.
-// See Lecture 50, part 1
-for (var i = 0; i < names.length; i++) {
-
-  // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
-  // Retrieve the first letter of the current name in the loop.
-  // Use the string object's 'charAt' function. Since we are looking for
-  // names that start with either upper case or lower case 'J'/'j', call
-  // string object's 'toLowerCase' method on the result so we can compare
-  // to lower case character 'j' afterwards.
-  // Look up these methods on Mozilla Developer Network web site if needed.
-  var firstLetter = names[i].charAt(0).toLowerCase();
-
-  // STEP 12: (NOTHING TO DO. ALREADY DONE FOR YOU)
-  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
-  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
-  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-  // name in the loop.
-  if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
-  } else {
-    helloSpeaker.speak(names[i]);
+    if (firstLetter === "j") {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
   }
-}
+  console.log(" "); // To add row between first and second list prints
+  var mappingNames = function (oneName) {
+    var firstLetter = oneName.charAt(0).toLowerCase();
 
+    if (firstLetter === "j") {
+      return byeSpeaker.speakSimple(oneName);
+    } else {
+      return helloSpeaker.speakSimple(oneName);
+    }
+  };
+
+  var mappedNamesVer3 = names.map(mappingNames);
+  for (greeting of mappedNamesVer3) {
+    console.log(greeting);
+  }
+
+  //**********************************************************
+  // Alternate method to achieve same output
+  //**********************************************************
+  // var mappedNames = names.map(function (n) {
+  //   for (var i = 0; i < n.length; i++) {
+  //     var firstLetter = n.charAt(0).toLowerCase();
+
+  //     if (firstLetter === "j") {
+  //       return byeSpeaker.speakSimple(n);
+  //     } else {
+  //       return helloSpeaker.speakSimple(n);
+  //     }
+  //   }
+  // });
+  // console.log(mappedNames);
+
+  //**********************************************************
+  // Another alternate method to achieve same output
+  //**********************************************************
+  // var mappedNamesVer2 = Array.prototype.map.call(names, mappingNames);
+  // for (greeting of mappedNamesVer2) {
+  //   console.log(greeting);
+  // }
+
+  //**********************************************************
+  // Alternate method to iterate through array
+  //**********************************************************
+  // mappedNamesVer2.forEach((element) => console.log(element));
 })();
