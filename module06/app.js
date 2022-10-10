f24ab30217a467feea4ddf3bc8f74d950bcb95be
rdlorenzo1972 <randy.lorenzo@gmail.com>
2022-10-10 18:31:36 -0400

    corrections

--- module06/app.js
+++ module06/app.js
@@ -3,13 +3,13 @@
 
   angular
     .modular("LunchCheck", [])
-
     .controller("LunchCheckController", LunchCheckController);
-
   LunchCheckController.$inject = ["$scope"];
   function LunchCheckController($scope) {
-    $scope.lunchItems = "fish, soda, ice cream, burger";
-    $scope.lunchMessage = "";
-    $scope.lunchButton = function () {};
+    $scope.lunchItems = ["fish", "soda", "ice cream", "burger"];
+    $scope.lunchMessage = "I'm very hungry";
+    $scope.lunchButton = function ($scope) {
+      $scope.lunchMessage = "Then eat";
+    };
   }
 })();
