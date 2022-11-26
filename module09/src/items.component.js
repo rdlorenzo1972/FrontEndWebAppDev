e5ca8c1e0257c0a72ca003041b8a4d40942031bf
rdlorenzo1972 <randy.lorenzo@gmail.com>
2022-11-25 23:06:54 -0500

    Add home html, but not able to get routes working

--- module09/src/items.component.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  angular.module("data").component("items", {
-    //templateUrl:
-    //controller <-- is this needed? Video shows adding here and removing from script
-    //bindings: {items: '<'}
-  });
-})();
