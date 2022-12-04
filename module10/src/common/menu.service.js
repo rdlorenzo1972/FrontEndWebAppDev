4ca0f0dc0caaa86194ed4be7a0e8940182e96a19
rdlorenzo1972 <randy.lorenzo@gmail.com>
2022-12-04 18:18:25 -0500

    Fav DISH string parsed to get category and cat item number

--- module10/src/common/menu.service.js
+++ module10/src/common/menu.service.js
@@ -23,19 +23,68 @@
 
     // Need to research REST structure
     // NOTE: Different structure than Module 09
-    // ApiPath / menu_itmes / c(cat short name) / menu_items / (menu number).json
+    // ApiPath / menu_items / c(cat short name) / menu_items / (menu number).json
     service.getMenuItem = function (shortName) {
       console.log("Inside MenuService, getmenuItem, looking for: " + shortName);
-      return $http
-        .get(ApiPath + "/menu_items/" + shortName + ".json")
-        .then(function (response) {
-          console.log("Retrieved fav item is: " + response);
-          return response.data;
-        })
-        .catch(function (response) {
-          console.log("Error retrieving favorite dish");
-          return;
-        });
+      var catShortname = service.getCategory(shortName);
+      var urlString =
+        ApiPath +
+        "/menu_items/" +
+        catShortname +
+        "/menu_items/" +
+        service.getCategoryItem(shortName, catShortname.length) +
+        ".json";
+      return (
+        $http
+          //.get(ApiPath + "/menu_items/" + shortName + ".json")
+          // var catShortname = service.getCategory(shortName);
+          .get(urlString)
+          .then(function (response) {
+            console.log("Retrieved fav item is: " + response);
+            return response.data;
+          })
+          .catch(function (response) {
+            console.log("Error retrieving favorite dish");
+            return;
+          })
+      );
+      // .then(function (response) {
+      //   console.log("Retrieved fav item is: " + response);
+      //   return response.data;
+      // })
+    };
+
+    service.getCategory = function (favDish) {
+      var favDishTrim = favDish.trim(); // trim(favDish);
+      var favDishCat = "";
+
+      // Either CAT short name is 1 or 2 letters, so will check is 2nd char
+      // is a ltter. If so, the cat short name has 2 letter short name.
+      if (favDishTrim.length > 1 && /[a-zA-Z]/.test(favDishTrim.charAt(1))) {
+        favDishCat = favDishTrim.substring(0, 2);
+      } else {
+        favDishCat = favDishTrim.substring(0, 1);
+      }
+      favDishCat = favDishCat.toUpperCase();
+      console.log("Determined favDish category short-name is: " + favDishCat);
+      return favDishCat;
+    };
+
+    service.getCategoryItem = function (favDish, catSize) {
+      var favDishTrim = favDish.trim(); // trim(favDish);
+      var favDishCatItem = "";
+
+      if (
+        favDishTrim.length > catSize + 1 &&
+        !/[a-zA-Z]/.test(favDishTrim.charAt(3))
+      ) {
+        favDishCatItem = favDishTrim.substring(catSize, catSize + 2);
+      } else {
+        favDishCatItem = favDishTrim.substring(catSize, catSize + 1);
+      }
+      //favDishCatItem = favDishCatItem.toUpperCase();
+      console.log("Determined favDish category item is: " + favDishCatItem);
+      return favDishCatItem;
     };
   }
 })();
